47a6263bf24638d203548df1e409fa5dd3031c2b
ikuno <jkolom@gmail.com>
2020-05-26 13:26:23 +0200

    Experimental support for Auth digest as per the solution in https://stackoverflow.com/a/60031108/1665217

--- 3GPP Meeting Helper/gui/config.py
+++ 3GPP Meeting Helper/gui/config.py
@@ -3,6 +3,7 @@ import server
 import gui.main
 from urllib.parse import urlparse, quote_plus
 import traceback
+from requests_digest_proxy import HTTPProxyDigestAuth
 
 # Wait for proxy settings
 # https://stackoverflow.com/questions/38678415/how-to-wait-for-response-from-modal-window-before-continuing-using-tkinter
@@ -30,7 +31,9 @@ class NetworkConfigDialog:
         self.proxy_password = tkinter.Entry(top, show='*')
         self.proxy_password.grid(row=2, column=1, columnspan=2, sticky="EW")
 
-        tkinter.Button(top, text="Use proxy and close window", command=self.ok).grid(row=3, column=1, sticky="EW")
+        self.auth_digest_chosen = tkinter.IntVar()
+        tkinter.Checkbutton(top, text="Digest Auth.", variable=self.auth_digest_chosen).grid(row=3, column=0, sticky="EW")
+        tkinter.Button(top, text="Use HTTP proxy and close window", command=self.ok).grid(row=3, column=1, sticky="EW")
         tkinter.Button(top, text="No proxy and close window", command=self.ko).grid(row=3, column=2, sticky="EW")
 
         tkinter.Label(top, text="Meeting HTTP server").grid(row=4, column=0)
@@ -46,22 +49,43 @@ class NetworkConfigDialog:
         
     def ok(self):
         # Setup a proxy
-        try:
-            user_password = ''
-            user = self.proxy_user.get().strip()
-            if len(user)>0:
-                the_password = self.proxy_password.get()
-                the_password = quote_plus(the_password)
-                user_password = '{0}:{1}@'.format(user, the_password)
-            o = urlparse(self.proxy_server.get())
-            proxies = {
-                'http': '{0}://{2}{1}'.format(o.scheme, o.netloc, user_password),
-                'https': '{0}://{2}{1}'.format(o.scheme, o.netloc, user_password)
-                }
-            server.non_cached_http_session.proxies = proxies
-        except:
-            print('Could not setup HTTP proxy')
-            traceback.print_exc()
+        if self.auth_digest_chosen.get() == 0:
+            print('Setting up proxy with basic authentication')
+            try:
+                user_password = ''
+                user = self.proxy_user.get().strip()
+                if len(user)>0:
+                    the_password = self.proxy_password.get()
+                    the_password = quote_plus(the_password)
+                    user_password = '{0}:{1}@'.format(user, the_password)
+                o = urlparse(self.proxy_server.get())
+                print('Using proxy {0}://{1}'.format(o.scheme, o.netloc))
+                proxies = {
+                    'http': '{0}://{2}{1}'.format(o.scheme, o.netloc, user_password),
+                    'https': '{0}://{2}{1}'.format(o.scheme, o.netloc, user_password)
+                    }
+                server.non_cached_http_session.proxies = proxies
+            except:
+                print('Could not setup HTTP proxy with Basic Authentication')
+                traceback.print_exc()
+        else:
+            print('Setting up proxy with digest authentication')
+            try:
+                user_password = ''
+                user = self.proxy_user.get().strip()
+                if len(user)>0:
+                    the_password = self.proxy_password.get()
+                    server.non_cached_http_session.auth = HTTPProxyDigestAuth(user, the_password)
+                proxy_url = self.proxy_server.get()
+                print('Using proxy {0}'.format(proxy_url))
+                proxies = {
+                    'http':  proxy_url,
+                    'https': proxy_url
+                    }
+                server.non_cached_http_session.proxies = proxies
+            except:
+                print('Could not setup HTTP proxy with Digest Authentication')
+                traceback.print_exc()
             
         self.top.destroy()
     def ko(self):
