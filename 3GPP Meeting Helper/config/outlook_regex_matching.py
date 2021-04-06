3caed6b63cffcea955aac9b8a15af07d015914ed
Josep Colom Ikuno <josep.colomikuno@t-mobile.at>
2021-04-06 12:36:33 +0200

    Improvements to simple mail moving

--- 3GPP Meeting Helper/config/outlook_regex_matching.py
+++ 3GPP Meeting Helper/config/outlook_regex_matching.py
@@ -13,4 +13,8 @@
 # [SA3#101-e][4.22] Final Status
 # Re: [SA3#102bis-e][2.10.5] TR 33.851-050 --- S3-211343 approved
 # [SA3#101-e][4.22] Final Status
-sa3 = r'\[(?P<meeting>.*)\][\s]*\[(?P<ai>[\d]+\.[\d]+(\.[\d]+)?)[,\s]*(?P<ai_title>[\w\s\.]*)?\][\s]*(?P<title>.*)'
+# [SA3#102bis-e][2.7, A&A][S3-211115] UAS: Update to solution #2
+# [SA3#102bis-e][1][S3 210811] E-meeting procedures
+# Not supported:
+# [SA3#102bis-e][2.17,sol2[S3-211091]AMF reallocation: Update to solution #2
+sa3 = r'\[(?P<meeting>.*)\][\s]*\[(?P<ai>[\d]+(\.[\d]+)?(\.[\d]+)?(\.[\d]+)?)[,\s]*(?P<ai_title>[\w\s\.\&]*)?\][\s]*(?P<title>.*)'
